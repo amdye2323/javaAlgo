3cfa7faf620fcaf27b8e6891b53d99fd57bd881b
KimTaeyoung <amdye2323@naver.com>
2022-05-30 23:54:06 +0900

    Changes

--- infrearn/twoArray/subject02.java
+++ infrearn/twoArray/subject02.java
@@ -1,22 +1,24 @@
 package infrearn.twoArray;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class subject02 {
 
     public ArrayList<Integer> solution(int n,int m ,int[] a ,int[] b){
+        int p1 = 0,p2=0;
+        Arrays.sort(a);
+        Arrays.sort(b);
         ArrayList<Integer> answer = new ArrayList<>();
 
-        for(Integer x:a){
-            for(Integer y:b){
-                if(x==y){
-                    answer.add(x);
-                }
-            }
+        while (p1<n && p2<n){
+            if(a[p1] == b[p2]){
+                answer.add(a[p1++]);
+                p2++;
+            }else if(a[p1]<b[p2])p1++;
+            else p2++;
         }
-
-        Collections.sort(answer);
 
         return answer;
     }
